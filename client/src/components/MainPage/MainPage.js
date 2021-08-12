e3356d6149f02c7b267f787a58b0fc0199f825d8
Nafisa Valieva <nafisalenva@gmail.com>
2021-08-12 15:22:55 +0300

    Little changes

--- client/src/components/MainPage/MainPage.js
+++ client/src/components/MainPage/MainPage.js
@@ -27,7 +27,7 @@ const MainPage = () => {
                                 <h1 className="hero-title">Онлайн спорт с персональным тренером.</h1>
                                 <span className="hero-description">Занимайтесь спортом, <span className="green-text">не выходя из дома,</span> по любой удобной программе</span>
                                 <Link to="/enter" className="nav__list-item">
-                                    <button className="enter-btn-hero">Войти</button>
+                                    <button className="enter-btn-hero">Попробовать бесплатно</button>
                                 </Link>
                             </div>
                             <div className="hero-images">
@@ -49,29 +49,30 @@ const MainPage = () => {
                                                 <span className="about-us__head">Удобная платформа для спорта все-в-одном</span>
                                                 <p className="about-us__desc">Всё проводится на нашей платформе. Вам не придётся заходить на сторонние сервисы</p>
                                             </div>
-                                            <img src="/about-image.png" alt="" className="about-img" />    
+                                            {/* <img src="/about-image.png" alt="" className="about-img" />     */}
                                         </div>
                                         <div className="about-us__item">
                                             <div className="about-us__texts">
                                                 <span className="about-us__head">1:1 Онлайн тренировки</span>
                                                 <p className="about-us__desc">Личные тренер подберёт все процессы индивидуально под вас</p>
                                             </div>
-                                            <img src="/about-image.png" alt="" className="about-img"/>    
+                                            {/* <img src="/about-image.png" alt="" className="about-img"/>     */}
                                         </div>
                                         <div className="about-us__item">
                                             <div className="about-us__texts">
                                                 <span className="about-us__head">Сертифицированные специалисты</span>
                                                 <p className="about-us__desc">На платформе специалисты с опытом работы более 2 лет</p>
                                             </div>
-                                            <img src="/about-image.png" alt="" className="about-img"/>    
+                                            {/* <img src="/about-image.png" alt="" className="about-img"/>     */}
                                         </div>
                                         <div className="about-us__item">
                                             <div className="about-us__texts">
                                                 <span className="about-us__head">Доступные цены</span>
                                                 <p className="about-us__desc">Онлайн-тренировки не только легче, но и дешевле. При этом вы не теряете эффективности тренировок.</p>
                                                 <b className="about-us__desc">Тренировки от 600 руб!</b>
+                                                <b className="about-us__desc">Попробовать бесплатно</b>
                                             </div>
-                                            <img src="/about-image.png" alt="" className="about-img"/>    
+                                            {/* <img src="/about-image.png" alt="" className="about-img"/>     */}
                                         </div>
                                 </Carousel>
                                 
