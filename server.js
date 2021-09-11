09a13fa84579c859048311863807aed8ef15c7f8
Varukhin Vladimir <varukhin04@mail.ru>
2021-09-11 16:35:11 +0300

    Changing upload middleware to multer-s3

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const express = require("express");
 const logger = require("morgan");
 const config = require("config");
 const mongoose = require("mongoose");
-const multer = require("multer")
+const multer = require("multer");
+const multerS3 = require("multer-s3")
+const aws = require("aws-sdk");
 const app = express();
 
 require('dotenv').config()
@@ -16,21 +18,30 @@ app.get("*", function (req, res, next) {
     }
 });
 
+
+
 app.use("/images", express.static(path.join(__dirname, "public/images")));
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "public/images")
+const s3 = new aws.S3({})
+
+const storage = multerS3({
+    s3: s3,
+    bucket: "soft-platform",
+    metadata: function (req, file, cb) {
+        cb(null, {fieldName: file.fieldname});
     },
-    filename: (req, file, cb) => {
+    key: function (req, file, cb) {
         cb(null, req.body.id
             + "_"
             + req.body.typeName
             + (req.body.index ? "_" + req.body.index : "")
             + "."
-            + file.originalname.split(".")[1])
+            + file.originalname.split(".")[1]
+        )
     }
-});
+})
+
+
 
 const upload = multer({storage: storage});
 
@@ -72,6 +83,13 @@ async function start() {
             useUnifiedTopology: true,
             useCreateIndex: true
         })
+        await aws.config.getCredentials(function(err) {
+            if (err) console.log(err.stack);
+            // credentials not loaded
+            else {
+                console.log("Access key:", aws.config.credentials.accessKeyId);
+            }
+        });
         http.listen(PORT, () => console.log(`On air. Port: ${PORT}`))
     } catch (e) {
         console.log('Error', e.message)
